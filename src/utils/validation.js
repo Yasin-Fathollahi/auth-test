7811e7b16ee5f59865a6e40d10d74e450af37d9d
Yasin Fathollahi <yasin.fathollahi82@gmail.com>
2025-09-08 23:27:43 +0330

    fixed +98 validation

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,8 +1,10 @@
 export function validatePhoneNumber(phoneNum) {
+  const isNumber = typeof +phoneNum === 'number';
+
   if (
-    typeof +phoneNum === 'number' &&
+    isNumber &&
     ((phoneNum.startsWith('09') && phoneNum.length === 11) ||
-      (phoneNum.startsWith('+989') && phoneNum.length === 13) ||
+      (phoneNum.startsWith('989') && phoneNum.length === 12) ||
       (phoneNum.startsWith('00989') && phoneNum.length === 14))
   ) {
     return { error: null };
